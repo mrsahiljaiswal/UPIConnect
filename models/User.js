58ed5198e200b81c4ee1264f6bb05374c58e72e6
mrsahiljaiswal <sahiljaiswal757@gmail.com>
2025-05-09 18:34:54 +0530

    added Notification

--- models/User.js
+++ models/User.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  username: { type: String, unique: true, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  balance:{type:Number,default :10000},
+  notifications: [
+    {
+      message: { type: String, required: true },
+      type: { type: String, enum: ["request_sent", "request_accepted", "request_rejected"], required: true },
+      transactionId: { type: mongoose.Schema.Types.ObjectId, ref: "Transaction", required: false },
+      seen: { type: Boolean, default: false },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
