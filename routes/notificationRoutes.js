ddacaf79e5f0989aa1593bce61daa5ad81b5305e
mrsahiljaiswal <sahiljaiswal757@gmail.com>
2025-05-11 05:04:31 +0530

    added mark-all-as-seen feature

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const router = express.Router();
-const { markAsRead, dismissNotification, getNotifications } = require("../controllers/notificationController");
+const {
+  getNotifications,
+  markAllAsSeen,
+  markAsRead,
+  dismissNotification,
+} = require("../controllers/notificationController");
 const authenticate = require("../middleware/authMiddleware");
+const router = express.Router();
 
-router.get("/", authenticate, getNotifications); // Fetch notifications
+// Fetch all notifications
+router.get("/", authenticate, getNotifications);
+
+// Mark all notifications as seen
+router.patch("/mark-all-as-seen", authenticate, markAllAsSeen);
+
+// Mark a specific notification as read
 router.patch("/mark-as-read/:id", authenticate, markAsRead);
+
+// Dismiss a specific notification
 router.delete("/dismiss/:id", authenticate, dismissNotification);
 
 module.exports = router;
